39d7bbba7baf059d200e92b705b19bd09ccb3e53
cvukxwhoo <chienvuk@gmail.com>
2023-10-12 02:28:22 +0700

    2nd commit

--- src/components/Layout/DefaultLayout/Header/index.jsx
+++ src/components/Layout/DefaultLayout/Header/index.jsx
@@ -1,11 +1,72 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import classNames from "classnames/bind";
+import styles from "./Header.module.scss";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faCartShopping,
+  faMagnifyingGlass,
+  faBagShopping,
+} from "@fortawesome/free-regular-svg-icons";
 
+const cx = classNames.bind(styles);
 function Header() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>
+        {/* Logo */}
+        <div className={cx("box-logo")}>
+          <Link to="/">
+            <img
+              src="https://entrada.webtravel.vn/image/logo-entrada.png"
+              alt="entrada"
+            />
+          </Link>
+        </div>
+        {/* Links */}
+        <div className={cx("navs")}>
+          <ul style={{ margin: "0" }}>
+            <li>
+              <Link className={cx("nav-child")} to="/">
+                Home
+              </Link>
+              <Link className={cx("nav-child")} to="/destination">
+                Destination
+              </Link>
+              <Link className={cx("nav-child")} to="/listings">
+                Listings
+              </Link>
+              <Link className={cx("nav-child")} to="/activities">
+                Activities
+              </Link>
+              <Link className={cx("nav-child")} to="/shop">
+                Shop
+              </Link>
+              <Link className={cx("nav-child")} to="/blog">
+                Blog
+              </Link>
+              <Link className={cx("nav-child")} to="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+          {/* Components */}
+          <div className="box-cta">
+            <FontAwesomeIcon icon={faUser} />
+            <FontAwesomeIcon icon={faCartShopping} />
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+            <FontAwesomeIcon icon={faBagShopping} />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
+
+{
+  /* <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <div className="main-header-logo">
           <Navbar.Brand href="#home">Entrada</Navbar.Brand>
@@ -14,7 +75,9 @@ function Header() {
         <div className="main-header-content">
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Link to="/">Home</Link>
+              <Nav.Link>
+                <Link to="/">Home</Link>
+              </Nav.Link>
               <Link to="/destination">Destination</Link>
               <Link to="/listing">Listings</Link>
               <Link to="/activities">Activities</Link>
@@ -26,8 +89,5 @@ function Header() {
           </Navbar.Collapse>
         </div>
       </Container>
-    </Navbar>
-  );
+    </Navbar> */
 }
-
-export default Header;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,19 @@
 import Home from "../pages/Home";
 import Destination from "../pages/Destination";
-import React from "react";
+import Listing from "../pages/Listing";
+import Activities from "../pages/Activities";
+import Shop from "../pages/Shop";
+import Blog from "../pages/Blog";
+import Contact from "../pages/Contact";
 
 const publicRoutes = [
   { path: "/", component: Home },
   { path: "/destination", component: Destination },
+  { path: "/listings", component: Listing },
+  { path: "/activities", component: Activities },
+  { path: "/shop", component: Shop },
+  { path: "/blog", component: Blog },
+  { path: "/contact", component: Contact },
 ];
 const privateRoutes = [];
 export { publicRoutes, privateRoutes };
